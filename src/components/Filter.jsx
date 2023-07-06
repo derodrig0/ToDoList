aa5c918fa72629d1cb1cea9a1cf99401d00f4063
Rodrigo <derodrigo@gmail.com>
2023-07-06 16:27:58 -0300

    Lista de tarefas finalizada

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,15 +1,24 @@
-const Filter = () => {
+const Filter = ({ filter, setFilter, setSort }) => {
   return (
     <div className="filter">
       <h2>Filtrar:</h2>
       <div className="filter-options">
         <div>
           <p>Status:</p>
-          
+          <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+            <option value="All">Todos</option>
+            <option value="Completed">Completos</option>
+            <option value="incompleteed">Incompletos</option>
+          </select>
+        </div>
+        <div>
+          <p>Ordem Alfabética:</p>
+          <button onClick={() => setSort("Asc")}>Asc ▼</button>
+          <button onClick={() => setSort("Desc")}>Desc ▲</button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Filter
+export default Filter;
